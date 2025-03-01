17bd723ce93ded5742ec3d183a07a53ad045f3c7
Kasyful Haq B. <82114912+DatBoiSUS-Baka@users.noreply.github.com>
2025-03-01 15:52:38 +0700

    Update Main.py

--- Python/Program/Main.py
+++ Python/Program/Main.py
@@ -2,4 +2,16 @@ from Baju import Baju
 
 listBarang = []
 
-listBarang.append(Baju())
+listBarang.append(Baju("001", "Kalung", 20000, 15, "Kalung",
+                       "Leather", "Coklat", "Kucing/Anjing",
+                       "M", "Gucci"))
+listBarang.append(Baju("002", "BajuKucing", 15000, 10,
+                       "BajuHewan", "Katun", "Merah", "Kucing",
+                       "M", "Gucci"))
+listBarang.append(Baju("003", "Pasir", 30000, 30,
+                       "Pasir", "Pasir", "Abu", "Any", "L",
+                       "LifeCat"))
+listBarang.append(Baju("004", "MakananKucing", 12500, 20, "Makanan",
+                       "Tuna", "Coklat", "Kucing", "M", "Whiskas"))
+listBarang.append(Baju("005", "MainanAnjing", 10000, 15, "Mainan",
+                       "Plastik", "Hijau", "Anjing", "S", "LifeCat"))
